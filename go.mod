7ad5228dcc8b5898d2e6329692691cd588a3a2ea
Rameez Sajwani <rameezwazirali@hotmail.com>
2023-09-14 23:51:02 -0700

    adding getdrive method

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/yusufpapurcu/wmi v1.2.3
 	gopkg.in/fsnotify.v1 v1.4.7
 )
 
@@ -13,6 +14,7 @@ require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"gopkg.in/fsnotify.v1"
+	"github.com/yusufpapurcu/wmi"
 )
 
 func main() {
@@ -18,6 +19,12 @@ func main() {
 		ctx.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Hello from Go and Gin running on Azure App Service",
 			"link":  "/json",
+		})
+	})
+
+	router.GET("/json", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"foo": "bar",
 		})
 	})
 
@@ -67,4 +74,56 @@ func main() {
 	}
 
 	router.Run("127.0.0.1:" + port)
+}
+
+type Win32_DiskDrive struct {
+	DeviceID          string
+	MediaType         string
+	Model             string
+	InterfaceType     string
+	Partitions        uint32
+	Size              uint64
+	TotalCylinders    uint64
+	TotalHeads        uint64
+	TotalSectors      uint64
+	TotalTracks       uint64
+	TracksPerCylinder uint32
+	SectorsPerTrack   uint32
+	BytesPerSector    uint32
+}
+
+func getDrives() []Win32_DiskDrive {
+	var ssdList []Win32_DiskDrive
+	//query := "SELECT DeviceID, MediaType, Model, InterfaceType FROM Win32_DiskDrive WHERE MediaType='SSD'"
+	query := "SELECT DeviceID, MediaType, Model, InterfaceType FROM Win32_DiskDrive"
+	err := Query(query, &ssdList)
+	if err != nil {
+		fmt.Printf("Query failed: %s/n", err)
+		return []Win32_DiskDrive{}
+	}
+
+	/*fmt.Println("List of SSDs:")
+	for _, ssd := range ssdList {
+		fmt.Printf("Device ID: %s\n", ssd.DeviceID)
+		fmt.Printf("Model: %s\n", ssd.Model)
+		fmt.Printf("Interface Type: %s\n", ssd.InterfaceType)
+		fmt.Printf("Media Type: %s\n", ssd.MediaType)
+		fmt.Println("----------------------------------------------------")
+	}*/
+	return ssdList
+}
+
+func toJson(ssdList []Win32_DiskDrive) string {
+	if len(ssdList) == 0 {
+		return "no data"
+	}
+	var jsonResult string
+	for _, ssd := range ssdList {
+		fmt.Printf("Device ID: %s\n", ssd.DeviceID)
+		fmt.Printf("Model: %s\n", ssd.Model)
+		fmt.Printf("Interface Type: %s\n", ssd.InterfaceType)
+		fmt.Printf("Media Type: %s\n", ssd.MediaType)
+		fmt.Println("----------------------------------------------------")
+	}
+	return jsonResult
 }
